615def538f6986818fd2672f944c1246c893a72d
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2023-04-14 12:18:55 +0200

    Mostrare i dati di un array con ngFor in una tabella html

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class UsersComponent {
 
+  title = 'Users';
+  users = [
+    {
+      name: 'Daniele1',
+      lastname: 'Segreto1',
+      email: 'daniele@gmail.com',
+      fidcalcode: 'SGTDAN77J33X777Z',
+      province: 'Palermo',
+      phone: '3334455678',
+      age: 33
+    },
+    {
+      name: 'Daniele2',
+      lastname: 'Segreto2',
+      email: 'daniele@gmail.com',
+      fidcalcode: 'SGTDAN77J33X777Z',
+      province: 'Palermo',
+      phone: '3334455678',
+      age: 33
+    },
+    {
+      name: 'Daniele3',
+      lastname: 'Segreto3',
+      email: 'daniele@gmail.com',
+      fidcalcode: 'SGTDAN77J33X777Z',
+      province: 'Palermo',
+      phone: '3334455678',
+      age: 33
+    },
+    {
+      name: 'Daniele4',
+      lastname: 'Segreto4',
+      email: 'daniele@gmail.com',
+      fidcalcode: 'SGTDAN77J33X777Z',
+      province: 'Palermo',
+      phone: '3334455678',
+      age: 33
+    },
+  ]
+
 }

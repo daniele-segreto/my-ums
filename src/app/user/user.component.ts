379a442c726ce1cb600572545508b3450b8209c5
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2023-04-14 17:40:09 +0200

    Passare dati da un componente padre a un figlio

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-user',
+  selector: 'tr[app-user]', // selezione un elemento 'tr' che abbia come attributo [app-user]
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent {
+  // RICEVO I DATI DAL COMPONENTE PADRE:
+  // Ricevo in input dei valori attraverso quest variabile dall'esterno
+  @Input('user-data') user: any; // 'user-data' è un alias esterno
 
 }

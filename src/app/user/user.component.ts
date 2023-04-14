a74eeb1ac976e873ae07177dfc14d2ccc0499c5b
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2023-04-14 18:12:10 +0200

    Gestire eventi nativi in un componente

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,4 +10,14 @@ export class UserComponent {
   // Ricevo in input dei valori attraverso quest variabile dall'esterno
   @Input('user-data') user: any; // 'user-data' è un alias esterno
 
+  // Non è necessario passare il parametro user, lo abbiamo già ricevuto in @Input (sopra)
+  // deleteUser(user: any) {
+  //   alert(user.lastname)
+  // }
+
+  // Quindi accediamo al parametro user tramite il this
+  deleteUser() {
+    alert(this.user.lastname)
+  }
+
 }

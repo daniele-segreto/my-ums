cd62982c901eafce990379089242d76e955d0e30
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2023-05-11 15:05:35 +0200

    Jsonserver. Eliminare un utente lato client con @ViewChildren

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,39 +1,57 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, Output, QueryList, ViewChildren } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { User } from '../classes/User';
 import { Observable } from 'rxjs';
+import { UserComponent } from '../user/user.component';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, AfterViewInit { // implemento anche AfterViewInit
   title = 'Users';
 
   public users$: Observable<User[]> = this.service.getUsers();
-  public users: User[] = []; // Inizializzo la variabile 'users' con un array vuoto di oggetti 'User' (serve per il METODO 2)
+  // public users: User[] = []; // non serve più (era un esempio)
 
   @Output() updateUser = new EventEmitter<User>();
 
+  // @ViewChildren(UserComponent) trs!: QueryList<UserComponent>;
+
+  // Utilizzo il decoratore @ViewChildren in Angular per ottenere un riferimento
+  // a tutti gli elementi UserComponent presenti all'interno di un componente.
+  // La proprietà 'read' indica che l'oggetto restituito dalla QueryList sarà di tipo ElementRef.
+  @ViewChildren(UserComponent, {read: ElementRef}) trs!: QueryList<ElementRef>;
+  // '!' vuol dire che trs non sarà né 'null', ne 'undefined', quindi non verrà segnalato come errore
+
   constructor(private service: UserService) {}
 
-  ngOnInit(): void {
-    // *METODO 2:
-    this.service.getUsers().subscribe(res => this.users = res);
+  // Il metodo ngAfterViewInit() viene chiamato quando la vista del componente è stata completamente inizializzata
+  ngAfterViewInit(): void {
+    // Stampa sulla console il messaggio "afterViewInit" e la lista di riferimenti agli elementi ottenuta tramite il decoratore @ViewChildren
+    console.log('afterViewInit', this.trs);
+    // this.trs.forEach(ele => console.log(ele)); // lo metto in: 'onDeleteUser'
   }
 
-  // Questo codice cancella l'utente dal database e ricarica la pagina
+  ngOnInit(): void {
+    // this.service.getUsers().subscribe(res => this.users = res); // non serve più (era un esempio)
+  }
+
+  // METODO 3:
   onDeleteUser(user: User) {
-    this.service.deleteUser(user).subscribe(res => { // Si sottoscrive all'Observable restituito dal metodo 'deleteUser' del servizio per eliminare l'utente dal database
-
-      // METODO 1: utilizzando sul file html: <tbody *ngIf="(users$ | async) as users">
-      // location.reload(); // Ricarica la pagina dopo la cancellazione dell'utente
-
-      // *METODO 2: togliendo l'*ngIf dal <tbody>
-      // Questo codice richiede la lista degli utenti dal server e la assegna alla variabile 'users'
-      this.service.getUsers().subscribe(res => this.users = res);
-      // Si sottoscrive all'Observable restituito dal metodo 'getUsers' del servizio per ottenere la lista degli utenti dal server. Quando la risposta arriva, assegna la lista degli utenti alla variabile 'users'.
+    // Viene chiamato il metodo deleteUser() dal servizio (passando l'utente da eliminare)
+    this.service.deleteUser(user).subscribe(res => {
+      // this.service.getUsers().subscribe(res => this.users = res); non serve più (era un esempio)
+      // Viene iterata la lista di elementi UserComponent ottenuta tramite il decoratore @ViewChildren
+      this.trs.forEach(ele => {
+        // Per ciascun elemento della lista viene ottenuto l'oggetto nativo corrispondente
+        const el = ele.nativeElement;
+        // Se l'id dell'elemento corrisponde all'id dell'utente che si desidera eliminare...
+        if (Number(el.id) === user.id) {
+          el.parentNode.removeChild(el); //...l'elemento viene rimosso dal DOM
+        }
+      });
     });
   }
 

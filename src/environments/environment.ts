f5b44e25de547b389d4120d299a95945d201a001
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2023-05-23 18:37:55 +0200

    modifica apiurl in environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // Definizione della costante di ambiente "environment"
 export const environment = {
   production: false, // La proprietà "production" è impostata su "false"
-  APIURL: 'http://localhost:3000/users' // La proprietà "APIURL" contiene l'URL di un endpoint API
+  // APIURL: 'http://localhost:3000/users' // La proprietà "APIURL" contiene l'URL di un endpoint API (funziona con json-server)
+  APIURL: 'http://localhost:8000/api/users' // Funziona con laraapi (progetto Laravel)
 };
 
 // Questo codice definisce una costante di ambiente utilizzata in un'applicazione web. La costante specifica che l'applicazione non è in modalità di produzione e contiene l'URL di un endpoint API. In questo caso, l'endpoint API è in ascolto sulla porta 3000 del server locale.

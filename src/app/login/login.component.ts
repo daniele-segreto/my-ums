1faa4de69b47188e20b093cc9fac7d2fcfe1c3f9
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2023-07-25 18:07:14 +0200

    Auth signin e localStorage

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms'; // Importo ngFormn
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +10,29 @@ import { NgForm } from '@angular/forms'; // Importo ngFormn
 })
 export class LoginComponent implements OnInit{
 
-  constructor() {}
+  // Inietto AuthService e Router
+  constructor(private auth: AuthService, private router: Router) {}
 
   ngOnInit() { }
 
   // Metodo di accesso chiamato quando il form viene inviato
-  signIn(form: NgForm) {
-    alert(form.valid); // Avviso per mostrare se il form è valido o meno
-    alert(form.value.email); // Avviso per mostrare il valore dell'email inserita
+  signIn(form: NgForm): boolean {
+    // Verifica se il form è valido, se non lo è, restituisce false e non procede con il login
+    if (!form.valid) {
+      return false;
+    }
+
+    // Il form è valido, quindi procede con il login chiamando la funzione di signIn e passando i valori di email e password dal form
+    let result = this.auth.signIn(form.value.email, form.value.password);
+
+    // Controlla il risultato del login
+    if (result) {
+      // Se il login è riuscito (result è true), naviga verso la pagina principale ('').
+      this.router.navigate(['']);
+    }
+
+    // Restituisce true, indicando che il login è stato effettuato con successo
+    return true;
   }
+
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,28 +4,43 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  // Variabile che serve a verificare se l'utente è loggato (true) o no (false)
-  private isUserLogged = true;
+  // Variabile privata che serve a verificare se l'utente è loggato (true) o no (false)
+  private isUserLogged = false; // Viene inizializzata a false quando viene creato l'oggetto
 
   constructor() { }
 
-  // Logica che serve a capire se l'utente è loggato
+  // Metodo che controlla se l'utente è loggato o meno
   isUserLoggedIn() {
+    // Verifica se esiste un valore chiamato 'token' nello storage locale
+    // !! è usato per convertire il valore restituito da getItem in un valore booleano
+    this.isUserLogged = !!localStorage.getItem('token');
+    // Restituisce il valore corrente di isUserLogged (true se l'utente è loggato, altrimenti false)
     return this.isUserLogged;
   }
 
-  // Metodo con cui l'utente farà il login
+  // Metodo per il login dell'utente, richiede email e password come parametri
   signIn(email: string, password: string) {
+    // Avviso temporaneo per verificare se l'email e la password vengono ricevute correttamente (per debug)
+    // alert(email + ', ' + password);
 
+    // Verifica se esiste il token nello storage locale, inserendo l'email come valore del token
+    localStorage.setItem('token', email);
+
+    // Restituisce true, indicando che il login è avvenuto con successo
+    return true;
   }
 
-  // Metodo con cui l'utente fa la registrazione
+  // Metodo per la registrazione dell'utente, richiede username, email e password come parametri
   signUp(username: string, email: string, password: string) {
-
+    // code...
   }
 
-  // Metodo con cui l'utente farà il logout
+  // Metodo per il logout dell'utente
   logout() {
+    // Rimuove il valore 'token' dallo storage locale
+    localStorage.removeItem('token');
+
+    // Imposta la variabile isUserLogged a false per indicare che l'utente non è più loggato
     this.isUserLogged = false;
   }
 }
